cc6db5b992697d48e40e2e6687cb4e69cf77e7be
danilobraga <naril88@gmail.com>
2016-09-14 19:25:25 -0300

    removed invalid files

--- src/main/java/com/dbraga/springrest/app/dao/PersonDAO.java
+++
@@ -1,9 +0,0 @@
-package com.dbraga.springrest.app.dao;
-
-import java.util.List;
-
-import com.dbraga.springrest.app.domain.Person;
-
-public interface PersonDAO extends GenericDAO<Person, Long> {
-	public List<Person> getAll();
-}
